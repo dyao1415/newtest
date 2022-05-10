dbf7b4f329db720d87f0d1a23ebe091e43cc2483
dyao1415 <davidyao1645@gmail.com>
2022-05-10 13:30:28 -0500

    test

---
+++ main.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from dataclasses import dataclass
+from urllib import request
+import urllib
+
+def is_Important(link: str) -> bool:
+
+    f = urllib.request.urlopen(link)
+    opened_file = f.read()
+    if "keyword" in opened_file.decode('utf-8'):
+        return True
+    else:
+        return False
+
+
+with open("urls") as f:
+    myline = f.readline()
+    while myline:
+        print(is_Important(myline))
+        myline = f.readline()
+
